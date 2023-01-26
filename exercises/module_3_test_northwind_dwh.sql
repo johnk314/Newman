e468d54db5b944cc1bc2de7ad84fcdf3269dbc8b
John C. King V <johnk314@gmail.com>
2023-01-26 09:07:17 -0600

    Northwind DWH tests.

--- exercises/module_3_test_northwind_dwh.sql
+++ exercises/module_3_test_northwind_dwh.sql
@@ -1,1 +1,34 @@
 -- Test that the Northwind database installed correctly.
+
+SELECT TOP(10) *
+FROM [Northwind].[dbo].[Categories];
+
+SELECT TOP(10) * 
+FROM [Northwind].[dbo].[Customers];
+
+SELECT TOP(10) * 
+FROM [Northwind].[dbo].[Employees];
+
+SELECT TOP(10) * 
+FROM [Northwind].[dbo].[EmployeeTerritories];
+
+SELECT TOP(10) * 
+FROM [Northwind].[dbo].[Order Details];
+
+SELECT TOP(10) * 
+FROM [Northwind].[dbo].[Orders];
+
+SELECT TOP(10) * 
+FROM [Northwind].[dbo].[Products];
+
+SELECT TOP(10) * 
+FROM [Northwind].[dbo].[Region];
+
+SELECT TOP(10) * 
+FROM [Northwind].[dbo].[Shippers];
+
+SELECT TOP(10) * 
+FROM [Northwind].[dbo].[Suppliers];
+
+SELECT TOP(10) * 
+FROM [Northwind].[dbo].[Territories];
