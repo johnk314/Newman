e47d9fd7f501d3799a9f1a6abe2a223a6039c130
John C. King V <johnk314@gmail.com>
2023-04-29 12:32:55 -0500

    Update Module #8.

--- exercises/module_8_views.sql
+++ exercises/module_8_views.sql
@@ -1,30 +1,65 @@
--- QUESTION #1: Write a query to join & denormalize/flatten Product, Category, & Supplier tables. Create a view of the query named EmployeesOrdersCustomers.
--- HINT: Once created successfully, you'll see the view to the left under Connections > Servers > localhost > Databases > Northwind > Views. Try refreshing the views folder.
--- HINT: If you need to delete the view to start over, un-comment the DROP VIEW statement below.
+-- QUESTION #1: Write a query to join & denormalize/flatten Product, Category, & Supplier tables. Create a view of the join named EmployeesOrdersCustomers.
+-- HINT: You can see the view under CONNECTIONS > SERVERS > localhost > Databases > Northwind > Views.
+-- HINT: You may need to refresh the database/views folder.
 -- HINT: Once the view has been created, comment out that code & move on to QUESTION #2.
 
+-- Set current database to Northwind. (otherwise the view will be created in System Databases > Master)
+USE Northwind 
+GO
+
+-- Create view for question #1.
 CREATE VIEW EmployeesOrdersCustomers AS 
     SELECT 
-        *
+        O.OrderID,
+        O.CustomerID,
+        O.EmployeeID,
+        CAST(O.OrderDate AS DATE) AS OrderDate,
+        CAST(O.RequiredDate AS DATE) AS RequiredDate,
+        CAST(O.ShippedDate AS DATE) AS ShipDate,
+        O.ShipVia,
+        O.Freight,
+        O.ShipName,
+        O.ShipAddress,
+        O.ShipCity,
+        O.ShipRegion,
+        
+        C.CompanyName,
+        C.ContactName,
+        C.ContactTitle,
+        C.Address AS CustomerAddress,
+        C.City AS CustomerCity,
+        C.Region AS CustomerRegion,
+        C.PostalCode AS CustomerPostalCode,
+        C.Country AS CustomerCountry,
+        
+        E.LastName,
+        E.FirstName,
+        E.Title,
+        E.BirthDate,
+        E.HireDate,
+        E.Address AS EmployeeAddress,
+        E.City AS EmployeeCity,
+        E.Region AS EmployeeRegion,
+        E.PostalCode AS EmployeePostalCode,
+        E.Country AS EmployeeCountry
+
     FROM 
         [Northwind].[dbo].[Orders] AS O
-        -- LEFT JOIN Customers AS C
-        -- LEFT JOIN Employees AS E
+            LEFT JOIN Customers AS C
+            LEFT JOIN Employees AS E;
 
--- DROP VIEW IF EXISTS EmployeesOrdersCustomers;
+-- If needed, delete the EmployeesOrdersCustomers view.
+-- DROP VIEW EmployeesOrdersCustomers;
 
+SELECT *
+FROM EmployeesOrdersCustomers;
 
--- QUESTION #2: Which suppliers have the most Products in Northwind's catalog?
--- HINT: We're interested in the number of Orders, not the dollar-value.
--- HINT: We're interest in total Orders, not by year, etc.
-
--- SELECT *
--- FROM EmployeesOrdersCustomers;
+-- QUESTION #2: How many rows are in the Orders table? How many rows are in the EmployeesOrdersCustomers view?
 
 
+-- QUESTION #3: Which Customers (CompanyName) placed the most orders in 1998?
+-- HINT: Each row is one order.
 
--- QUESTION #3: Which categories have more products in 1998 than 1997?
--- HINT: Try using EmployeesOrdersCustomers in a sub-query.
 
--- SELECT *
--- FROM EmployeesOrdersCustomers;
+-- QUESTION #4: Which Employees placed the most Orders?
+
